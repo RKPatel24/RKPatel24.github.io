47f5e6e42b73f842e21c43480bcac292eea8ab3f
RKPatel24 <kaneriyaraj133@gmail.com>
2023-11-02 16:18:17 +0530

    New ADD
    Completed

---
+++ grid/js/glowing.js
@@ -0,0 +1,23 @@
+
+span = document.getElementById('menubars');
+span1 = document.getElementById('menubars1');
+submenu = document.getElementById('submenu');
+
+
+
+function test() {
+    span.style.visibility = "hidden";
+    span1.style.visibility = "visible";
+    submenu.style.display = "block";
+}
+ 
+function test1() {
+    span1.style.visibility = "hidden";
+    span.style.visibility = "visible";
+    submenu.style.display = "none";
+}
+
+
+
+    
+
